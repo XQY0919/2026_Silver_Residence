6506cf03d70e9586fc0ec54b87fa45403b0d9c69
徐淇源 <2711135776@qq.com>
2026-03-26 18:47:01 +0800

    Initial commit

---
+++ src/test/java/org/example/xqy1/_026_silver_residence/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xqy1._026_silver_residence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
